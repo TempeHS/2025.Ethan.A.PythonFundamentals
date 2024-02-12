8884ae5001ac2f7f1ffc78a1461acf5066bffe76
EthanFAndrews <158523809+EthanFAndrews@users.noreply.github.com>
2024-02-12 01:50:58 +0000

    incomplete interpreter.py

---
+++ 1-Conditionals/interpreter/interpreter.py
@@ -0,0 +1,19 @@
+def main():
+    equation = input("input an equation")
+    (
+        x,
+        y,
+        z,
+    ) = equation.split(" ")
+    x_f = float(x)
+    z_f = float(z)
+
+    if y == ("+"):
+        answer = x_f + z_f
+    if y == ("-"):
+        answer = x_f - z_f
+    if y == ("/"):
+        answer = x_f / z_f
+
+
+main()
