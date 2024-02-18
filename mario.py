74bbba2b611eca78aaea1f92ca3597ee4650fc40
EthanFAndrews <158523809+EthanFAndrews@users.noreply.github.com>
2024-02-18 07:16:45 +0000

    completed mario.py

--- mario.py
+++ mario.py
@@ -3,13 +3,8 @@ def main():
 
 
 def print_square(size):
-    # For each row in square
     for i in range(size):
-        # For each brick in row
-        for j in range(size):
-            # print brick
-            print("#", end="")
-        print()
+        print("#" * size)
 
 
 main()
