21f26d3553ae650fc8bab263cf968548e27d0473
EthanFAndrews <158523809+EthanFAndrews@users.noreply.github.com>
2024-02-23 03:11:06 +0000

    completed updated rps.py

--- rps.py
+++ rps.py
@@ -3,29 +3,32 @@ def computer_choice():
 
 
 def player_choice():
-    player_choice = input("Choose rock, paper or scissors:   ").lower()
-    match computer_choice():
-        case "rock":
-            if player_choice == "rock":
-                print("You Tie")
-            if player_choice == "paper":
-                print("You Win")
-            if player_choice == "scissors":
-                print("You Lose")
-        case "scissors":
-            if player_choice == "rock":
-                print("You Win")
-            if player_choice == "paper":
-                print("You Lose")
-            if player_choice == "scissors":
-                print("You Tie")
-        case "paper":
-            if player_choice == "rock":
-                print("You Lose")
-            if player_choice == "paper":
-                print("You Tie")
-            if player_choice == "scissors":
-                print("You Win")
+    games_played = 0
+    while games_played < 3:
+        games_played = games_played + 1
+        player_choice = input("Choose rock, paper or scissors:   ").lower()
+        match computer_choice():
+            case "rock":
+                if player_choice == "rock":
+                    print("You Tie")
+                elif player_choice == "paper":
+                    print("You Win")
+                elif player_choice == "scissors":
+                    print("You Lose")
+            case "scissors":
+                if player_choice == "rock":
+                    print("You Win")
+                elif player_choice == "paper":
+                    print("You Lose")
+                elif player_choice == "scissors":
+                    print("You Tie")
+            case "paper":
+                if player_choice == "rock":
+                    print("You Lose")
+                elif player_choice == "paper":
+                    print("You Tie")
+                elif player_choice == "scissors":
+                    print("You Win")
 
 
 player_choice()
