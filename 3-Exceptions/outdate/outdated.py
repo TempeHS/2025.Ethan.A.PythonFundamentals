48702a7b1d9002476c62bd4a95b3495d90f0dcd5
EthanFAndrews <158523809+EthanFAndrews@users.noreply.github.com>
2024-03-11 08:26:03 +0000

    Upload completed outdated.py

---
+++ 3-Exceptions/outdate/outdated.py
@@ -0,0 +1,32 @@
+month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+while True:
+    date = input("Please input the date: ")
+    try:
+        if "/" in date:
+            mm, dd, yyyy = date.split("/")
+        elif "," in date:
+            mmdd, yyyy = date.split(", ")
+            mm, dd = mmdd.split(" ")
+            # No need to check if mm in months. KeyError is handled separately.
+            mm = (month.index(mm)) + 1
+        if int(mm) > 12 or int(dd) > 31:
+            raise ValueError
+    except NameError:
+        pass
+    else:
+        print(f"{int(yyyy)}-{int(mm):02}-{int(dd):02}")
+        break
